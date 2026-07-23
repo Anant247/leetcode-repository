fac01dc54a3049598e71ff5ba5e73b4842cf4ea0
Anant Koli <ganeshkoli4041c@gmail.com>
2026-07-23 22:08:16 +0530

    Time: 3 ms (73.84%), Space: 45.7 MB (70.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,39 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> target = new HashMap<>();
-        for(char c : t.toCharArray()){
-            target.put(c, target.getOrDefault(c,0) + 1);
+        if (s.length() < t.length()) return "";
+        int[] count = new int[128];
+
+        for (char c : t.toCharArray()) {
+            count[c]++;
         }
-        
-        HashMap<Character, Integer> current = new HashMap<>();
-        
+
         int low = 0;
         int start = 0;
-        int res = Integer.MAX_VALUE;
-        
-        for (int high = 0; high < s.length(); high++){
-            current.put(s.charAt(high), current.getOrDefault(s.charAt(high), 0) +1);
-            while (charcontain (current, target)){
-                if(res > high - low +1){
-                    res = high - low +1;
+        int minlen = Integer.MAX_VALUE;
+        int needs = t.length();
+
+        for(int high = 0; high<s.length(); high++){
+            if (count[s.charAt(high)] > 0) {
+               needs--;
+            }
+            count[s.charAt(high)]--;
+
+            while(needs == 0){
+                if(minlen > high - low + 1){
+                    minlen = high - low + 1;
                     start = low;
                 }
-                current.put(s.charAt(low), current.get(s.charAt(low)) -1);
-                if(current.get(s.charAt(low)) == 0){
-                    current.remove(s.charAt(low));
+                count[s.charAt(low)]++;
+
+                if (count[s.charAt(low)] > 0) {
+                    needs++;
                 }
                 low++;
             }
         }
-        if (res == Integer.MAX_VALUE)return "";
-        return s.substring(start, start + res);
-    }
-    public boolean charcontain (HashMap<Character,Integer> current, HashMap<Character, Integer> target){
-        for(Character c : target.keySet()){
-            
-            if(!current.containsKey(c)) return false;
-            if(current.get(c) < target.get(c)) return false;
-        }
-        return true;
+        return minlen == Integer.MAX_VALUE ? "" : s.substring(start, start + minlen); 
     }
 }
