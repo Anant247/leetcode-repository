8fcdda38c9ead33164e9cd124a3333c0d04f81cc
Anant Koli <ganeshkoli4041c@gmail.com>
2026-09-02 23:20:53 +0530

    Time: 0 ms (100%), Space: 45.2 MB (57.87%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+/*  if all elements in nums1 is even or odd then nums2 
+    will have all even or odd elements so return true
+      
+    if all elements in nums1 are either odd or even than
+    if nums2[i] = nums1[i] (even or odd)
+    than for any other i
+    nums2[i] = nums1[i] - nums[2]; will always odd or even 
+    ie. even − even = even
+        odd − odd = even
+        even − odd = odd
+        odd − even = odd
+      */ 
+    return true;
+    }
+}
